920c7b9d60ce2cae8cea15bdd0c15e5b0926e778
adausilio <alessandro.dausilio@suez.com>
2024-03-07 11:30:02 +0100

    obs_converter and forward operator first impl refs #1

---
+++ observations/obs_converters/S5P_TROPOMI_L3/work/quickbuild.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# DART software - Copyright UCAR. This open source software is provided
+# by UCAR, "as is", without charge, subject to all terms of use at
+# http://www.image.ucar.edu/DAReS/DART/DART_download
+
+main() {
+
+export DART=$(git rev-parse --show-toplevel)
+source "$DART"/build_templates/buildconvfunctions.sh
+
+CONVERTER=S5P_TROPOMI_L3
+LOCATION=threed_sphere
+EXTRA=obs_errors.path_names
+
+
+programs=(
+convert_s5p_tropomi_l3
+obs_seq_to_netcdf
+obs_sequence_tool
+advance_time
+)
+
+# build arguments
+arguments "$@"
+
+# clean the directory
+\rm -f -- *.o *.mod Makefile .cppdefs
+
+# build and run preprocess before making any other DART executables
+buildpreprocess
+
+# build 
+buildconv
+
+
+# clean up
+\rm -f -- *.o *.mod
+
+}
+
+main "$@"
