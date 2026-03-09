03b0a7b28ecf086695f10edd1b1490bb8522a1f6
a.dausilio <alessandro.dausilio@suez.com>
2026-03-09 13:18:16 +0100

    offset correct

--- mimesi_orch/pipelines/chimere2017/paths.py
+++ mimesi_orch/pipelines/chimere2017/paths.py
@@ -79,12 +79,12 @@ class Chimere2017Paths:
         mem: int,
         time_manager : TimeManager,
         prefix: str,
-        offset : 1
+        offset : int
         ) -> Path:
         if model != ModelType.CHIMERE:
             raise ValueError(f"Chimere2017Paths does not support model type: {model}")
         ts = time_manager.formatted_time(0, "%Y%m%d%H")
         
-        ts_p1 = time_manager.formatted_time(offset, "%Y%m%d%H")
+        ts_p_offset = time_manager.formatted_time(offset, "%Y%m%d%H")
 
-        return self.chimere_output_runs_dir(mem) / f"{prefix}.{ts}_{ts_p1}_ITA7.nc"
+        return self.chimere_output_runs_dir(mem) / f"{prefix}.{ts}_{ts_p_offset}_ITA7.nc"
