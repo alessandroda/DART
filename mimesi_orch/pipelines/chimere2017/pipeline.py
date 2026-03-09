cb9658a224b47657e90ce6a2742a1aef8020d4e2
a.dausilio <alessandro.dausilio@suez.com>
2026-03-09 22:53:54 +0100

    fix run_command_in_dir arg

--- mimesi_orch/pipelines/chimere2017/pipeline.py
+++ mimesi_orch/pipelines/chimere2017/pipeline.py
@@ -421,12 +421,13 @@ class Chimere2017DartPipeline(BaseAssimilationPipeline):
         )
         try:
             run_command_in_directory(
-                "convert_s5p_tropomi_l3",
-                self.paths.dart_s5p_work(),
+                CommandSpec(
+                    command="convert_s5p_tropomi_l3",
+                    directory=self.paths.dart_s5p_work(),
+                )
             )
         except Exception as e:
-            logger.error(f"Error running obs converter: {e}")
-            return False
+            raise FatalPipelineError(f"Error running obs converter: {e}") from e
 
         generated_obs_seq = [
             path
@@ -525,7 +526,7 @@ class Chimere2017DartPipeline(BaseAssimilationPipeline):
             subprocess.run(["cdo", "selname,pres,temp,spfc", temp_out_psfc, out_pres_t_spfc], check=True)
             end_file_with_mem = to_dart_dir / f"{end_file.stem}_{mem}.nc"
             subprocess.run(["cdo", f"selname,{self.ass_var}", end_ts, end_file_with_mem], check=True)
-            
+
             subprocess.run(
                 ["ncks", "-A", out_pres_t_spfc, end_file_with_mem],
                 check=True,
